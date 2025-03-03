dbf46568a3f97b1dafd4acafd9d793f28d70f348
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-03-03 16:44:39 +0530

    Verilog_SR_FlipFlop.v

---
+++ SR_FlipFlop.v
@@ -0,0 +1,16 @@
+// SR-FlipFlop using case statement
+module sr_flipflop (
+    input clk, 
+    input s, 
+    input r, 
+    output reg q
+);
+    always @(posedge clk) begin
+        case ({s, r})
+            2'b00: q <= q;  // No change
+            2'b01: q <= 0;  // Reset
+            2'b10: q <= 1;  // Set
+            2'b11: q <= 1'bx; // Invalid state
+        endcase
+    end
+endmodule
