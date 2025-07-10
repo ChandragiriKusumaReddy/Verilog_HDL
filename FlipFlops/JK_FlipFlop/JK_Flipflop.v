6ca4575e96f7c72d07d582ca4285ff43249a2834
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-10 19:38:42 +0530

    Verilog_JK_Flipflop.v

--- FlipFlops/JK_FlipFlop/JK_Flipflop.v
+++ FlipFlops/JK_FlipFlop/JK_Flipflop.v
@@ -14,3 +14,49 @@ module jk_flipflop (
         endcase
     end
 endmodule
+
+
+//Behavioral modeling
+module jk_fliflop(
+    input j,k,clk,rst,
+    output reg q,
+    output qn
+);
+    assign qn=~q;
+    always@(posedge clk or posedge rst) begin
+        if(rst)
+            q<=0;
+        else 
+            begin
+                case({j,k})
+                    2'b00: q<=q;  //No Change
+                    2'b01: q<=0;  //Reset
+                    2'b10: q<=1;  //Set
+                    2'b11: q<=~q;  //Toggle
+                endcase
+            end
+    end
+endmodule
+
+
+
+//Gate Level Modeling
+module jk_ff_gate(
+    input j,k,clk,rst,
+    output reg q,
+    output qn
+);
+    assign qn=~q;
+    always@(posedge clk or posedge rst) begin
+        if(rst)
+            q<=0;
+        else if(~j & ~k)
+            q<=q;   //No Change
+        else if(~j & k)
+            q<=0;   //Reset
+        else if(j & ~k)
+            q<=1;   //Set
+        else if(j & k)
+            q<=~q;  //Toggle
+    end
+endmodule
