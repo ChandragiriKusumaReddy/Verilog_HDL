991f9260f6374eb1da67825b405df5a92d49ba6a
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-10 19:21:06 +0530

    Verilog_D_FlipFlop.v

--- FlipFlops/D_FlipFlop/D_FlipFlop.v
+++ FlipFlops/D_FlipFlop/D_FlipFlop.v
@@ -1,3 +1,19 @@
+//D FLipflop with synchronous reset
+module d_fliflop_synchronous(
+    input clk,
+    input rst,
+    input d,
+    output reg q
+);
+    always@(posedge clk ) begin
+        if(rst)
+            q<=0;
+        else
+            q<=d;
+    end
+endmodule
+
+
 // D Flip-Flop with Asynchronous active-low Reset
 module d_flipflop_async_reset (
     input clk, 
