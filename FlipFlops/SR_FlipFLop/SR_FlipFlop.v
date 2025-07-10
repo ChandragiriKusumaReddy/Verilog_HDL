8822c68711a3b312178865723fc3b26622700b03
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-10 19:58:38 +0530

    Verilog_SR_FlipFlop.v

--- FlipFlops/SR_FlipFLop/SR_FlipFlop.v
+++ FlipFlops/SR_FlipFLop/SR_FlipFlop.v
@@ -14,3 +14,47 @@ module sr_flipflop (
         endcase
     end
 endmodule
+
+
+//Behavioral Modeling
+module sr_ff_behavioral(
+    input s,r,clk,rst,
+    output reg q,
+    output qn
+);
+    assign qn=~q;
+    always@(posedge clk or posedge rst) begin
+        if(rst)
+            q<=0;
+        else
+            begin
+                case({s,r})
+                    2'b00: q<=q;  //No Change
+                    2'b01: q<=0;  //Reset
+                    2'b10: q<=1;  //Set
+                    2'b11: q<=1'bx  //Invalid State
+                endcase
+            end
+endmodule
+
+
+//Gate Level Modeling
+module sr_ff_gatelevel(
+    input s,r,clk,rst,
+    output reg q,
+    output qn
+);
+    always@(posedge clk or posedge rst) begin
+        if(rst)
+            q<=0;
+        else if(~s & ~r)
+            q<=q;  //No Change
+        else if(~s & r)
+            q<=0;  Reset
+        else if(s & ~r)
+            q<=1;  //Set
+        else if(s & r)
+            q<=1'bx;  //Invalid
+    end
+endmodule
+                        
