5d726e2db00df27c9303d8897653b2eb5af6acce
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-14 18:22:57 +0530

    Create up_counter.v

---
+++ Counters/Up_Counter/up_counter.v
@@ -0,0 +1,13 @@
+module up_counter(
+  input clk,
+  input rst,
+  output [3:0] count
+);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        count<=4'b0000;
+      else
+        count<=count+1;
+    end
+endmodule
