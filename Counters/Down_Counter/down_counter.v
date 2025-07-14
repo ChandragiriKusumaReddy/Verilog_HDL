067c6a876a370640df4b16e714a79f66013b5c7b
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-14 18:29:31 +0530

    Create down_counter.v

---
+++ Counters/Down_Counter/down_counter.v
@@ -0,0 +1,13 @@
+module down_counter(
+  input clk,
+  input rst,
+  output [3:0] count
+);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        count<=4'b1111;
+      else
+        count<=count-1;
+    end
+endmodule
