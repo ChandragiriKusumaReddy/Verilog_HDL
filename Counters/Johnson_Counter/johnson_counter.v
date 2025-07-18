202f59e0b9bd7622519a281ea51ee1876c48fe99
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-18 19:12:26 +0530

    Update johnson_counter.v

--- Counters/Johnson_Counter/johnson_counter.v
+++ Counters/Johnson_Counter/johnson_counter.v
@@ -11,3 +11,21 @@ module twisted_ring_counter(
         q<={q[2:0],~q[3]}; 
     end
 endmodule
+
+
+
+
+//For n-bit
+module johnson_counter #(parameter N=4)(
+  input clk,
+  input rst,
+  output reg [N-1:0]q
+);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        q<=0;
+      else
+        q<={~q[N-1],q[N-1:1]};
+    end
+endmodule
