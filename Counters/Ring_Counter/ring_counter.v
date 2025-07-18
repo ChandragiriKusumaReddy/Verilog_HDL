c8d0cf35f112635d9a0b0e02ad69ba9ff2e0d641
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-18 19:15:35 +0530

    Update ring_counter.v

--- Counters/Ring_Counter/ring_counter.v
+++ Counters/Ring_Counter/ring_counter.v
@@ -11,3 +11,19 @@ module ring_counter(
         q<={q[2:0],q[3]}; //circular left shift
     end
 endmodule
+
+
+//For N-bit
+module ring_counter #(parameter N=4)(
+  input clk,
+  input rst,
+  coutput reg[N-1:0]
+);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        q<=1; //Initial pattern:000.....0001
+      else
+        q<={q[N-2:0],q[N-1]};
+    end
+endmodule
