ad138fdc9d108200c1f7b10a7e19cd4aa1c89771
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-18 19:35:18 +0530

    Update up_down_counter.v

--- Counters/Up_Down_Counter/up_down_counter.v
+++ Counters/Up_Down_Counter/up_down_counter.v
@@ -14,3 +14,20 @@ module up_down_counter(
         count<=count-1;
     end
 endmodule
+
+
+//Using Conditional Operator
+module up_down_counter(
+  input clk,
+  input rst,
+  input up_down,
+  output reg[3:0] count
+);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        count<=0;
+      else
+        count<=up_down?count+1:count-1;
+    end
+endmodule
